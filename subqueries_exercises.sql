efa2f0a78493b2fd4405d39f7fcad306378f62d3
Stanley Burke <stanley.burke08@gmail.com>
2023-10-20 13:55:42 -0500

    reviewed

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -2,27 +2,28 @@ use employees;
 select database();
 
 -- 1
-SELECT first_name, last_name
+SELECT first_name, last_name /* add to_date for currency */
 FROM employees
-WHERE hire_date IN (
+join dept_emp
+	using(emp_no)
+WHERE hire_date = (
     SELECT hire_date
     FROM employees
+    WHERE emp_no = '101010'
 ) 
-and hire_date = hire_date
-LIMIT 10;
+and to_date > now()
+;
 
 
 select hire_date,emp_no
-
 from employees;
-
 SELECT hire_date
 FROM employees
 WHERE emp_no = 101010;
 
 
   -- 2  
-SELECT e.first_name, t.title
+SELECT distinct t.title /*e.first_name*/
 FROM employees as e
 join titles as t
 	on e.emp_no = t.emp_no
@@ -31,7 +32,8 @@ join dept_emp as de
 WHERE  e.first_name in (
     SELECT first_name
     FROM employees
-    WHERE first_name = "Aamod") and de.to_date > now();
+    WHERE first_name = "Aamod")
+    and de.to_date > now();
     
     
 	SELECT first_name
@@ -50,19 +52,19 @@ WHERE  e.first_name in (
 --     
     
  -- 3  answer
-SELECT (select count(emp_no)
+SELECT (select count(emp_no) /* look at other solution */
    from employees) - (select count(emp_no)
     FROM dept_emp
     WHERE to_date > now()) as "total employees gone";
-   
-   select count(emp_no)
-   from employees;
-   
-   select count(e.emp_no)
-   from employees as e
-   join dept_emp as de
-	on e.emp_no = de.emp_no
-   WHERE de.from_date < now();
+    
+--    select count(emp_no)
+--    from employees;
+--    
+--    select count(e.emp_no)
+--    from employees as e
+--    join dept_emp as de
+-- 	on e.emp_no = de.emp_no
+--    WHERE de.from_date < now();
    
 -- 4
 SELECT e.first_name, e.last_name, e.gender
@@ -71,13 +73,13 @@ join dept_manager as dm
 	on e.emp_no = dm.emp_no
 WHERE dm.emp_no in (
     SELECT emp_no
-    FROM dept_manager) and e.gender = 'F';
+    FROM dept_manager) and e.gender = 'F' and to_date > now();
     
 -- 5
-select avg(s.salary)
-from salaries as s;
+-- select avg(s.salary)
+-- from salaries as s;
 
-SELECT e.first_name, e.last_name, s.salary
+SELECT count(*) /* fix this one */
 FROM employees as e
 join dept_emp as de
 	on e.emp_no = de.emp_no
@@ -85,23 +87,53 @@ join salaries as s
  on e.emp_no=s.emp_no
 WHERE s.salary > (
     select avg(salary)
-from salaries) and de.to_date > now()
-order by s.salary desc
-limit 10;
+from salaries) and de.to_date > now() and s.to_date > now();
+
+
+
+select count(*)
+from salaries
+where to_date > now() 
+	AND salary > 
+		(
+		select round(avg(salary),2)
+		from salaries        
+        );
     
 -- 6
-select max(salary)
-from salaries;
+-- select std(salary)
+-- from salaries
+-- where to_date >now();
 
-select count(salary)
+-- select max(salary)
+-- from salaries
+-- where to_date >now();
+
+select count(*)
 from salaries
-where (select max(salary)
-from salaries) between (.68 * (select salary
-    FROM salaries) (.68 * (select salary
-    FROM salaries)
-    and to_date > now());
+where salary >=(select max(salary) - std(salary) as cutoff
+from salaries
+where to_date > now()
 
-    
+) and to_date > now();
+
+select count(*) from salaries
+where to_date > now();
+
+-- select max(salary) - std(salary)
+-- from salaries
+-- where to_date > now();
+select((select count(*) /* percentage calculation */
+from salaries
+where salary >=(select max(salary) - std(salary) as cutoff
+from salaries
+where to_date > now()
+
+) and to_date > now())/(select count(*) from salaries
+where to_date > now())) * 100;
+
+select count(*) from salaries
+where to_date > now();
     
 -- bonus
 -- 1
